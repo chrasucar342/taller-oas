49d02ffc7d12fc20044c67d1bbba4b000c68c27b
chrasucar342 <christian.asuerocarrelln6@gmail.com>
2023-05-12 11:59:09 +0200

    Archivos subidos

---
+++ projection-homes-stats/controllers/projectionhomessController.js
@@ -0,0 +1,10 @@
+import * as service from '../services/projectionhomessService.js';
+
+export function getProjectionhomess(req, res) {
+    service.getProjectionhomess(req, res);
+}
+
+export function findByyear(req, res) {
+    service.findByyear(req, res);
+}
+
---
+++ projection-homes-stats/services/projectionhomessService.js
@@ -0,0 +1,14 @@
+export function getProjectionhomess(req, res) {
+    res.send({
+        province: "Cádiz",
+        year: 2008
+    });
+}
+
+export function findByyear(req, res) {
+    res.send({
+        province: "Cádiz",
+        year: 2008
+    });
+}
+
